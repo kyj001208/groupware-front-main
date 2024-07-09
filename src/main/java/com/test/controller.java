71cb68bc027212d44f52ac6a285f3fcbd1b79a50
mugy2 <mugy2@admin>
2024-07-09 22:32:54 +0900

    "feat: 팀 프로젝트 -프론트엔드 메인"

---
+++ src/main/java/com/test/controller.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class controller {
+	
+	@GetMapping("/p-main")
+	public String main() {
+		return "/p-main.html";
+	}
+	
+
+}
